3d81e24dc1e6b570c30263755a013fd7a70b875c
SDuck4 <cotmddn4@gmail.com>
2019-08-20 14:17:17 +0900

    Add isActive prop to Square

--- src/Square.js
+++ src/Square.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
 function Square(props) {
+  let className = 'square';
+  if (props.isActive) {
+    className += ' active';
+  }
+
   return (
-    <button className="square" onClick={props.onClick}>
+    <button 
+      className={className}
+      onClick={props.onClick}
+    >
       {props.value}
     </button>
   );
